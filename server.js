8a95e51f41be25c4c1858e671f09d33e7fec6871
Md. Shohel Rana <cse_shohel@yahoo.com>
2017-05-17 17:29:01 +0600

    updated vs code setting

--- server.js
+++ server.js
@@ -1,25 +1,43 @@
 "use strict";
-var express     = require('express'),
-    bodyParser  = require('body-parser'),
-    fs          = require('fs'),
-    app         = express();
+var express = require('express'),
+    bodyParser = require('body-parser'),
+    fs = require('fs'),
+    router = express.Router(),
+    app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 //Would normally copy necessary scripts into src folder (via grunt/gulp) but serving
 //node_modules directly to keep everything as simple as possible
-app.use('/node_modules', express.static(__dirname + '/node_modules')); 
+app.use('/node_modules', express.static(__dirname + '/node_modules'));
 
 //The src folder has our static resources (index.html, css, images)
-app.use(express.static(__dirname + '/src')); 
+app.use(express.static(__dirname + '/src'));
+
+app.use(function(req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, content-type, Authorization');
+  next();
+});
 
 // redirect all others to the index (HTML5 history)
-app.all('/*', function(req, res) {
+app.all('/*', function (req, res) {
     res.sendFile(__dirname + '/src/index.html');
 });
 
-app.listen(process.env.PORT || 3000, function() {
+app.use("/", router);
+router.use(function (req, res, next) {
+    console.log("/" + req.method);
+    next();
+});
+
+app.use("*", function (req, res) {
+    res.sendFile(__dirname + "/src/404.html");
+});
+
+app.listen(process.env.PORT || 3000, function () {
     console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
 
