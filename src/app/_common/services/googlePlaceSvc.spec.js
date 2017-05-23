dda767c0f6cd1eec0020d8e5768f5a5f9a118035
Md. Shohel Rana <cse_shohel@yahoo.com>
2017-05-23 07:30:30 +0600

    test configurations setting

---
+++ src/app/_common/services/googlePlaceSvc.spec.js
@@ -0,0 +1,6 @@
+describe('Google Palce Factory', function () {
+    it('has a dummy spec to test 2 + 2', function () {
+        // An intentionally failing test. No code within expect() will never equal 4.
+        expect(2 + 2).toEqual(4);
+    });
+});
